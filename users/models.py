66b1d443702d62676c5e27d859d86bfbc27cc559
partrick <orel.228.337@gmail.com>
2026-01-11 00:14:46 +0200

    added user model (before migrations)

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from managers import UserManager
+
+
+class User(AbstractBaseUser):
+    email = models.TextField(unique=True)
+    # is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ""
+
+    def __str__(self):
+        return f"{self.pk} {self.email}"
